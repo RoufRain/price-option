7128a8f6c2862e2f998b1d4d98143e8a1edad82d
RoufRain <roufrain91@gmail.com>
2023-09-19 15:07:43 +0600

    price option project with react,daisyui,tailwind and responsive navbar

---
+++ price-option/src/App.jsx
@@ -0,0 +1,20 @@
+
+import './App.css'
+import DaisyNav from './components/daisynav'
+
+function App() {
+  
+
+  return (
+    <>
+    <DaisyNav> </DaisyNav>
+     
+          <h1 className='text-7xl bg-red-500'>Vite + React</h1>
+
+
+
+    </>
+  )
+}
+
+export default App
---
+++ price-option/src/components/DaisyNav.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const DaisyNav = () => {
+    return (
+        <div>
+            <h3>navigation bar</h3>
+        </div>
+    );
+};
+
+export default DaisyNav;
